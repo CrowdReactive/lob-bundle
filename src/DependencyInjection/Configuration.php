7f6fcad20c5caaf160b7f972e1bbd16303ddf0a6
Ross <ross@crowdreactive.com>
2016-02-11 11:45:29 +0000

    Initial import

---
+++ src/DependencyInjection/Configuration.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace CrowdReactive\LobBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+final class Configuration implements ConfigurationInterface
+{
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $root = $treeBuilder->root('lob', 'array');
+
+        $root
+            ->children()
+                ->scalarNode('api_key')
+                    ->isRequired()
+                    ->cannotBeEmpty()
+                    ->info('Lob.com API key')
+                    ->end()
+                ->scalarNode('version')
+                    ->defaultNull()
+                    ->info('Version of the Lob.com API to use (default is latest)')
+                    ->end();
+
+        return $treeBuilder;
+    }
+}
---
+++ tests/DependencyInjection/CrowdReactiveLobExtensionTest.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace CrowdReactive\LobBundle\Tests\DependencyInjection;
+
+use CrowdReactive\LobBundle\DependencyInjection\CrowdReactiveLobExtension;
+use Lob\Lob;
+use Symfony\Component\Config\Definition\Exception\InvalidConfigurationException;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class CrowdReactiveLobExtensionTest extends \PHPUnit_Framework_TestCase
+{
+    /** @var ContainerBuilder */
+    private $container;
+
+    /** @var CrowdReactiveLobExtension */
+    private $extension;
+
+    public function setUp()
+    {
+        $this->container = new ContainerBuilder();
+        $this->extension = new CrowdReactiveLobExtension();
+    }
+
+    public function tearDown()
+    {
+        unset($this->container, $this->extension);
+    }
+
+    public function testWithDefaults()
+    {
+        $configs = [
+            'lob' => [
+                'api_key' => 'abc123',
+            ],
+        ];
+
+        $this->extension->load($configs, $this->container);
+
+        $this->assertInstanceOf(Lob::class, $this->container->get('lob'));
+
+        /** @var Lob $lob */
+        $lob = $this->container->get('lob');
+        $this->assertEquals('abc123', $lob->getApiKey());
+        $this->assertNull($lob->getVersion());
+    }
+
+    public function testWithoutApiKey()
+    {
+        $configs = [
+            'lob' => [],
+        ];
+
+        $this->expectException(InvalidConfigurationException::class);
+
+        $this->extension->load($configs, $this->container);
+    }
+
+    public function testWithVersion()
+    {
+        $configs = [
+            'lob' => [
+                'api_key' => 'abc123',
+                'version' => '1.6.0',
+            ],
+        ];
+
+        $this->extension->load($configs, $this->container);
+
+        /** @var Lob $lob */
+        $lob = $this->container->get('lob');
+        $this->assertEquals('1.6.0', $lob->getVersion());
+    }
+}
